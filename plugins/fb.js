ded6ca251516557b599f1d8d86bd19c546d4140b
kawshalamanupriya1-design <kawshalamanupriya1@gmail.com>
2026-07-31 22:31:18 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -9,7 +9,7 @@ cmd({
   filename: __filename
 }, async (conn, mek, m, { from, q, reply }) => {
   try {
-    if (!q) return reply("*AP NE KOI FACEBOOK VIDEO DOWNLOAD KARNI HAI 🤔 TO AP US FACEBOOK VIDEO KA LINK COPY KAR LO 🤗*\n*PHIR ESE LIKHO ☺️*\n\n*FB ❮FACEBOOK VIDEO LINK❯*\n\n*JAB AP ESE LIKHO GE 😇 TO APKI FACEBOOK VIDEO DOWNLOAD KAR KE 😃 YAHA PER BHEJ DE JAYE GE 😍♥️*");
+    if (!q) return reply("*FACEBOOK VIDEO DOWNLOAD 🤔 TO FACEBOOK VIDEO LINK COPY  🤗*\n*PESTE LIKE ☺️*\n\n*FB ❮FACEBOOK VIDEO LINK❯*\n\n* 😇 TO FACEBOOK VIDEO DOWNLOAD  😃 ENJOY ♥️*");
 
     const apiUrl = `https://movanest.xyz/v2/fbdown?url=${encodeURIComponent(q)}`;
     const res = await axios.get(apiUrl);
@@ -22,7 +22,7 @@ cmd({
 
     // 🔎 Results check
     if (!Array.isArray(data.results) || data.results.length === 0) {
-      return reply("*FACEBOOK VIDEO NAHI MIL RAHI 🥺*");
+      return reply("*FACEBOOK VIDEO NOT FOUND 🥺*");
     }
 
     const result = data.results[0];
@@ -33,14 +33,14 @@ cmd({
       : result.normalQualityLink;
 
     if (!videoUrl) {
-      return reply("*SIRF FACEBOOK VIDEO KA LINK DO ☺️*");
+      return reply("* FACEBOOK VIDEO LINK DO ☺️*");
     }
 
     // 📝 Caption API data se
     const caption = `*👑 FB VIDEO 👑*
 *👑 TIME :❯ ${result.duration}*
 *👑 CREATER :❯ ${data.creator}*
-*👑 BY :❯ ARSLAN-MD 👑*`;
+*👑 BY :❯ KAWSHALA-MD 👑*`;
 
     await conn.sendMessage(
       from,
